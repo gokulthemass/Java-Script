b95633dc60951bfb9125decb8596754a1c48c435
Gokula krishnan <112232586+gokulthemass@users.noreply.github.com>
2025-12-03 23:24:11 +0530

    Create end.ts

---
+++ angularJS/currency-exchange-app/end.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-end',
+  imports: [],
+  templateUrl: './end.html',
+  styleUrl: './end.css',
+})
+export class End {
+
+}

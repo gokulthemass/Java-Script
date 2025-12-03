641e40b69bc027fe26eb54486b8ae22fd9b8dc89
Gokula krishnan <112232586+gokulthemass@users.noreply.github.com>
2025-12-03 23:30:26 +0530

    Create body.ts

---
+++ angularJS/tech-quiz-app/body.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: 'app-body',                  
+  standalone: true,
+  templateUrl: './body.component.html',  
+  styleUrls: ['./body.component.css']    
+})
+export class Body {
+  
+ }                
